58dff5793477536f9db82df46ac8bc55edb5df32
simodoc22 <s340290@studenti.polito.it>
2025-11-25 12:46:34 +0100

    last try

--- model/model.py
+++ model/model.py
@@ -41,26 +41,40 @@ class Model:
 
         nuovo_dizionario_attrazioni = {}
         for i in lista_tour_attrazioni:
-            for j in i:
-                if j[0] not in nuovo_dizionario_attrazioni:
-                    id_tour = j[0]
-                    nuovo_dizionario_attrazioni[id_tour] = set()
-                    nuovo_dizionario_attrazioni[id_tour].add(j[1])
-                else:
-                    id_tour = j[0]
-                    nuovo_dizionario_attrazioni[id_tour].add(j[1])
+            if i["id_tour"] not in nuovo_dizionario_attrazioni:
+                id_tour = i["id_tour"]
+                oggetto_tour = self.tour_map[id_tour]
+                nuovo_dizionario_attrazioni[oggetto_tour] = set()
+                id_attrazione = i["id_attrazione"]
+                oggetto_attrazione = self.attrazioni_map[id_attrazione]
+                nuovo_dizionario_attrazioni[oggetto_tour].add(oggetto_attrazione)
+            else:
+                id_tour = i["id_tour"]
+                oggetto_tour = self.tour_map[id_tour]
+                id_attrazione = i["id_attrazione"]
+                oggetto_attrazione = self.attrazioni_map[id_attrazione]
+                nuovo_dizionario_attrazioni[oggetto_tour].add(oggetto_attrazione)
+
+        ##in questo modo ho dizionario id_tour: {oggetto attrazione 1, 2, 3}
+
+
         ##inoltre ogni attrazione ha un set di tour quindi creiamo un altro dizionario per avere la struttura dati
         ##anche per le attrazioni
         dizionario_tour = {}
         for i in lista_tour_attrazioni:
-            for j in i:
-                if j[1] not in dizionario_tour:
-                    id_attrazione = j[1]
-                    dizionario_tour[id_attrazione] = set()
-                    dizionario_tour[id_attrazione].add(j[0])
-                else:
-                    id_attrazione = j[1]
-                    dizionario_tour[id_attrazione].add(j[1])
+            if i["id_attrazione"] not in dizionario_tour:
+                id_tour = i["id_tour"]
+                id_attrazione = i["id_attrazione"]
+                oggetto_tour = self.tour_map[id_tour]
+                oggetto_attrazione = self.attrazioni_map[id_attrazione]
+                dizionario_tour[oggetto_attrazione] = set()
+                dizionario_tour[oggetto_attrazione].add(oggetto_tour)
+            else:
+                id_tour = i["id_tour"]
+                id_attrazione = i["id_attrazione"]
+                oggetto_tour = self.tour_map[id_tour]
+                oggetto_attrazione = self.attrazioni_map[id_attrazione]
+                dizionario_tour[oggetto_attrazione].add(oggetto_tour)
 
         return nuovo_dizionario_attrazioni, dizionario_tour
 
@@ -77,24 +91,82 @@ class Model:
         self.max_giorni = max_giorni
         self.max_budget = max_budget
 
-        self._pacchetto_ottimo, self._costo, self._valore_ottimo = self._ricorsione(0, [], 0, 0, 0, set(),self.id_regione,self.max_giorni,self.max_budget)
+        self._pacchetto_ottimo, self._costo, self._valore_ottimo = self._ricorsione([], 0, 0, 0, set(),self.id_regione,self.max_giorni,self.max_budget)
         return self._pacchetto_ottimo, self._costo, self._valore_ottimo
 
-    def _ricorsione(self, start_index: int, pacchetto_parziale: list, durata_corrente: int, costo_corrente: float,
-                        valore_corrente: int, attrazioni_usate: set,id_regione,giorni,budget):
-        """ Algoritmo di ricorsione che deve trovare il pacchetto che massimizza il valore culturale"""
-        tour_attrazioni, attrazioni_tour = self.load_relazioni()
 
-        if durata_corrente == giorni:
-            return pacchetto_parziale,valore_corrente,durata_corrente
-        else:
+    def calcolo_valore_culturale(self,attrazioni):
+        valore_culturale = 0
+        for i in attrazioni:
+            valore_culturale+=i.valore_culturale
+        return valore_culturale
+
+    def _ricorsione(self, pacchetto_parziale, durata_corrente, costo_corrente,
+                    valore_corrente, attrazioni_usate, id_regione, giorni, budget):
+
+        tour_attrazioni, _ = self.load_relazioni()
+
+        # ---- CASO TERMINALE ----
+        if durata_corrente > giorni or costo_corrente > budget:
+            return [], 0, 0
+
+        if valore_corrente > self._valore_ottimo:
+            self._valore_ottimo = valore_corrente
+            self._pacchetto_ottimo = pacchetto_parziale.copy()
+            self._costo = costo_corrente
+
+        best_pacchetto = pacchetto_parziale
+        best_valore = valore_corrente
+        best_costo = costo_corrente
+
+        for tour, attr_set in tour_attrazioni.items():
+
+            if str(tour.id_regione) != str(id_regione):
+                continue
+            if tour.durata_giorni + durata_corrente > giorni:
+                continue
+            if tour.costo + costo_corrente > budget:
+                continue
+
+
+            if len(attr_set.intersection(attrazioni_usate)) > 0:
+                continue
+
+
+            nuovo_valore = valore_corrente + sum(a.valore_culturale for a in attr_set)
+
+
+            nuove_attr_usate = attrazioni_usate.union(attr_set)
+
+
+            nuovo_pacchetto = pacchetto_parziale + [tour]
+
+            pac, cos, val = self._ricorsione(
+                nuovo_pacchetto,
+                durata_corrente + tour.durata_giorni,
+                costo_corrente + tour.costo,
+                nuovo_valore,
+                nuove_attr_usate,
+                id_regione,
+                giorni,
+                budget
+            )
+
+
+            if val > best_valore:
+                best_valore = val
+                best_pacchetto = pac
+                best_costo = cos
+
+        return best_pacchetto, best_costo, best_valore
 
 
 
 
 
 
-            pass
+
+
 
 
 
